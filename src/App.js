82d6c0c6ebf51aa2ec5f2ee54657747b1c6231a3
Patricio <concertmate@btinternet.com>
2019-05-04 16:53:41 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -8,14 +8,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends React.Component{
 render(){
   return(
-    <div className = "container">
-      <div className = "row">
-      <div className='col-6'>column number one </div>
-      <div className='col-6'>
-        <i className ='fas fa-home'/>
-       </div>
-      </div>
-    </div>
+<React.Fragment>
+  <h3>Hello from app</h3>
+</React.Fragment>
   );
 
   }
